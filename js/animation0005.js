686da1edc141d5489c2961097328c18b3131279b
wendellen <wendellenli@gmail.com>
2017-10-19 15:30:10 -0700

    gets rid of star at (0,0)

--- js/animation0005.js
+++ js/animation0005.js
@@ -1,32 +1,34 @@
-
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
+  canvas.position(0, 0);
 }
 
 function draw() {
+if (mouseX != 0 && mouseY != 0) {
   if (mouseIsPressed) {
-    fill('lightyellow');
-    stroke('pink');
-    strokeWeight(2);
-  } else {
     fill('pink');
     stroke('lightyellow');
     strokeWeight(2);
+    star(mouseX, mouseY, 20, 38, 5);
+  } else {
+      fill('lightyellow');
+      stroke('pink');
+      strokeWeight(2);
+      star(mouseX, mouseY, 20, 38, 5);
+    }
   }
-  star(mouseX, mouseY, 20, 38, 5);
 }
 
 function star(x, y, radius1, radius2, npoints) {
   var angle = TWO_PI / npoints;
-  var halfAngle = angle/2.0;
+  var halfAngle = angle / 2.0;
   beginShape();
   for (var a = 0; a < TWO_PI; a += angle) {
     var sx = x + cos(a) * radius2;
     var sy = y + sin(a) * radius2;
     vertex(sx, sy);
-    sx = x + cos(a+halfAngle) * radius1;
-    sy = y + sin(a+halfAngle) * radius1;
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
     vertex(sx, sy);
   }
   endShape(CLOSE);

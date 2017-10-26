908dace029b3ae5127ed0f1b4a41b652df07e7d2
wendellen <wendellenli@gmail.com>
2017-10-26 15:31:38 -0700

    loads first rhinestone

--- js/animation0006.js
+++ js/animation0006.js
@@ -1,6 +1,15 @@
 let butterfly;
-let rhinestone;
-let poem = [];
+let rhinestones = [];
+let poem = [flowers, that, fade, into, bloom];
+
+function preload() {
+  rhinestone1 = loadImage("../assets/rhinestone1.png")
+  rhinestone2 = loadImage("../assets/rhinestone2.png")
+  rhinestone3 = loadImage("../assets/rhinestone3.png")
+  rhinestone4 = loadImage("../assets/rhinestone4.png")
+  rhinestone5 = loadImage("../assets/rhinestone5.png")
+}
+
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
@@ -10,9 +19,12 @@ function setup() {
   mouseY = -1000
 }
 
-
-
 function draw() {
-  background(129, 112, 182);
+  background(255, 255, 255);
   butterfly.position(mouseX-270, mouseY-270);
+  // first rhinestone appears at page load
+  image(rhinestone1, random(100, 400), random(100, 400));
+  rhinestone1.resize(800, 0);
 }
+
+// new rhinestone appears upon mouse click

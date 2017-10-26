7fa7642ca01909187d2b69e19f983b75436913ac
wendellen <wendellenli@gmail.com>
2017-10-26 14:57:38 -0700

    animates butterfly cursor (YES!)

--- js/animation0006.js
+++ js/animation0006.js
@@ -1,14 +1,18 @@
-var img1
+let butterfly;
+let rhinestone;
+let poem = [];
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  img1 = createImg('assets/butterfly.gif');
+  butterfly = createImg('assets/butterfly.gif');
   mouseX = -1000
   mouseY = -1000
 }
 
+
+
 function draw() {
   background(129, 112, 182);
-  img1.position(mouseX-270, mouseY-270);
+  butterfly.position(mouseX-270, mouseY-270);
 }

baea214bf50588ad85b7d00bd557f1baa40bed1f
Crystal Emo <wendellenli@gmail.com>
2018-03-27 12:45:11 -0700

    changes 0001 back to original w out web audio

--- js/animation0001.js
+++ js/animation0001.js
@@ -1,47 +1,3 @@
-window.addEventListener("load", ()=>{
-  const audioContext = new AudioContext();
-  const triggerLevel = 0.99;
-
-  //setup analyser
-  const analyser = audioContext.createAnalyser();
-  analyser.fftSize = 128;
-
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array( bufferLength );
-
-  analyser.getByteTimeDomainData( dataArray );
-
-  var audioElement = document.querySelector("#my-audio");
-  console.log(audioElement);
-  var source = audioContext.createMediaElementSource(audioElement);
-
-  source.connect(analyser);
-  source.connect(audioContext.destination)
-
-  animate();
-
-  function animate() {
-
-    let didTrigger = false;
-
-  	analyser.getByteTimeDomainData( dataArray );
-
-  	for( var i = 0; i < bufferLength; i++ ) {
-
-  		let v = ( dataArray[ i ] / 128 );
-      if (v > triggerLevel) {
-        didTrigger = true;
-      }
-  	}
-    if (didTrigger) {
-      shape.stop();
-      shape.play();
-    }
-
-    console.log(didTrigger)
-    window.requestAnimationFrame(animate);
-  }
-})
 
 
 var shape = new mojs.Shape({
@@ -49,5 +5,5 @@ var shape = new mojs.Shape({
   scale: {0:3},
   duration: 2000,
   easing: 'cubic.out',
-  repeat: 0
-});
+  repeat: 999
+  }).play();
